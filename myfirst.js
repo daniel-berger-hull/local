f580d4539664b913429100f3b599c6d0fb0fb160
daniel-berger-hull <daniel.berger.hull@gmail.com>
2020-09-22 20:32:16 -0400

    Add a first file

---
+++ myfirst.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var url = require('url');
+
+var dt = require('./myfirstmodule');
+
+
+
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+
+//  var thisProcessName =  process.cwd();
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("<br>The date and time are currently: " + dt.myDateTime());
+  res.write("<br>The URL  is " + req.url);
+  //res.end("<br>" + txt);
+ // res.end("<br>The name of this process is " + thisProcessName);
+
+
+  res.end();
+}).listen(8080);
+
